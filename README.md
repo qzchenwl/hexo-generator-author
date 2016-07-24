989e958b7daa710dbc03481da6cae46c1bbf0f42
qzchenwl <qzchenwl@gmail.com>
2016-07-24 13:34:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# hexo-generator-author
+
+Author generator for [Hexo].
+
+## Installation
+
+``` bash
+$ npm install hexo-generator-author --save
+```
+
+## License
+
+MIT
+
+[Hexo]: http://hexo.io
+
---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var pagination = require('hexo-pagination');
+
+if (typeof Array.prototype.unique === 'undefined') {
+    Array.prototype.unique = function() { return Array.from(new Set(this)); };
+}
+
+hexo.extend.filter.register('template_locals', function(locals) {
+    if (typeof locals.site.authors === 'undefined') {
+        locals.site.authors = locals.site.posts.map(post => post.author).unique();
+    }
+});
+
+hexo.extend.helper.register('list_authors', function() {
+    const authors = this.site.authors.map(author => '<li class="author-list-item"><a class="author-list-link" href="AUTHOR">AUTHOR</a><span class="author-list-count">COUNT</span></li>'.replace(/AUTHOR/g, author).replace(/COUNT/g, this.site.posts.filter(post => post.author === author).length)).join('');
+
+    return '<ul class="author-list">AUTHORS</ul>'.replace(/AUTHORS/g, authors);
+});
+
+hexo.extend.generator.register("author", function(locals) {
+    const posts = locals.posts;
+    const authors = posts.map(post => post.author).unique().map(author => ({name: author, posts: posts.find({author})}));
+
+    return authors.reduce((result, author) => {
+        var posts = author.posts.sort('-date');
+        var data = pagination('authors/' + author.name, posts, {
+            layout: ['author', 'archive', 'index'],
+            data: {
+                author: author.name
+            }
+        });
+        return result.concat(data);
+    }, []);
+});
